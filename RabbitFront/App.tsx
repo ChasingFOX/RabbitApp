e1a87702f6a7e10dfa842e64458d9c37eb0233fa
shgus1224 <shgus1224@naver.com>
2022-09-20 09:26:12 -0400

    feat: 현재 위치 위도/경도 띄우기

--- RabbitFront/App.tsx
+++ RabbitFront/App.tsx
@@ -4,8 +4,9 @@ import {
   createNativeStackNavigator,
   NativeStackScreenProps,
 } from '@react-navigation/native-stack';
-import {Text, TouchableHighlight, View} from 'react-native';
-import {useCallback} from 'react';
+import {Text, TouchableHighlight, View, TouchableOpacity} from 'react-native';
+import {useCallback, useState} from 'react';
+import Geolocation from '@react-native-community/geolocation';
 
 type RootStackParamList = {
   Home: undefined;
@@ -14,13 +15,76 @@ type RootStackParamList = {
 type HomeScreenProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
 type DetailsScreenProps = NativeStackScreenProps<ParamListBase, 'Details'>;
 
+// function GeoLocationAPI() {
+//   const [latitude, setLatitude] = useState(String(''));
+//   const [longitude, setLogitude] = useState(String(''));
+
+//   const geoLocation = () => {
+//     Geolocation.getCurrentPosition(
+//       position => {
+//         const latitude = JSON.stringify(position.coords.latitude);
+//         const longitude = JSON.stringify(position.coords.longitude);
+
+//         setLatitude(latitude);
+//         setLogitude(longitude);
+//       },
+//       error => {
+//         console.log(error.code, error.message);
+//       },
+//       {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+//     );
+//   };
+
+//   return (
+//     <View>
+//       <Text> latitude: {latitude} </Text>
+//       <Text> longitude: {longitude} </Text>
+//       <TouchableOpacity
+//         onPress={() => geoLocation()}
+//         style={{backgroundColor: '#89B2E9'}}>
+//         <Text style={{color: 'white', textAlign: 'center'}}>
+//           Get GeoLocation Button
+//         </Text>
+//       </TouchableOpacity>
+//     </View>
+//   );
+// }
+
 function HomeScreen({navigation}: HomeScreenProps) {
+  const [latitude, setLatitude] = useState(String(''));
+  const [longitude, setLogitude] = useState(String(''));
+
+  const geoLocation = () => {
+    Geolocation.getCurrentPosition(
+      position => {
+        const latitude = JSON.stringify(position.coords.latitude);
+        const longitude = JSON.stringify(position.coords.longitude);
+
+        setLatitude(latitude);
+        setLogitude(longitude);
+      },
+      error => {
+        console.log(error.code, error.message);
+      },
+      {enableHighAccuracy: true, timeout: 50000, maximumAge: 10000},
+    );
+  };
+
   const onClick = useCallback(() => {
     navigation.navigate('Details');
   }, [navigation]);
 
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text> latitude: {latitude} </Text>
+      <Text> longitude: {longitude} </Text>
+      <TouchableOpacity
+        onPress={() => geoLocation()}
+        style={{backgroundColor: '#89B2E9'}}>
+        <Text style={{color: 'white', textAlign: 'center'}}>
+          Get GeoLocation Button
+        </Text>
+      </TouchableOpacity>
       <TouchableHighlight onPress={onClick}>
         <Text>Home Screen</Text>
       </TouchableHighlight>
@@ -43,6 +107,7 @@ function DetailsScreen({navigation}: DetailsScreenProps) {
 }
 
 const Stack = createNativeStackNavigator();
+
 function App() {
   return (
     <NavigationContainer>
