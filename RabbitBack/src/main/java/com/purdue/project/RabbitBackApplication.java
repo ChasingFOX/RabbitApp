b45af81ef756f816b18fa9b1f63e7502f36ccb97
zihooy139@gmail.com <zihooy139@gmail.com>
2022-09-21 15:32:17 -0400

    create back-end

---
+++ RabbitBack/src/main/java/com/purdue/project/RabbitBackApplication.java
@@ -0,0 +1,13 @@
+package com.purdue.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitBackApplication.class, args);
+	}
+
+}
