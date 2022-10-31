7247f0ec261598079c6c9b68111479b57bf7b53b
heewoneha <jhjmo0719h@naver.com>
2022-10-31 16:54:09 -0400

    feat: route function add

--- RabbitBack/src/main/java/com/purdue/project/model/Route.java
+++ RabbitBack/src/main/java/com/purdue/project/model/Route.java
@@ -1,16 +1,32 @@
 package com.purdue.project.model;
 
-import java.sql.Time;
+import java.sql.Timestamp;
 import java.util.ArrayList;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+
+import javax.persistence.*;
+
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "route")
 public class Route {
-    private Time time;
+    @Column
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer id;
+    @Column
+    private Timestamp time; // "time": "1592926896113",
+    @Column
     private String length;
+    @Column
     private Integer type;
-    private ArrayList<Location> waypoint = new ArrayList<Location>();
+    @Column
+    @ElementCollection
+    private ArrayList<Location> waypoint = new ArrayList<Location>(); // [{}, {}]
 
     @Override
     public String toString() {
