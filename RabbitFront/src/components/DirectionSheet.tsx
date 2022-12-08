1ed350bb851b43a2b5391aa84ed5280b16662fc0
YesHyeon <shgus1224@naver.com>
2022-12-08 10:45:24 -0500

    feat: Direction 페이지에서 버튼 클릭시 각각의 경로가 표시되도록 수정

--- RabbitFront/src/components/DirectionSheet.tsx
+++ RabbitFront/src/components/DirectionSheet.tsx
@@ -13,54 +13,40 @@ const DirectionSheet = ({route}: DircetionSheetProps) => {
   //   const route2 = useRoute<RouteProp<NaviPageParamList, 'Direction'>>();
   //   const navigation = useNavigation<NavigationProp<NaviPageParamList>>();
   const onNavigation = useCallback(() => {
-    // if (true) {
-    //   Geolocation.getCurrentPosition(
-    //     position => {
-    //       console.log(position);
-    //     },
-    //     error => {
-    //       // See error code charts below.
-    //       console.log(error.code, error.message);
-    //     },
-    //     {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
-    //   );
-    // }
-
     Linking.openURL(
       'https://www.google.com/maps/dir/?api=1&origin=40.42489539482597,-86.91051411560053&destination=40.473360126380996,-86.94642755184898&travelmode=walking&waypoints=40.42119341508705,-86.91781885879092%7C40.42732532443506,-86.92463136381483%7C40.43249524031551,-86.9269298077754%7C40.446337675508566,-86.92821177376851%7C40.45851363603605,-86.93213657343334%7C40.46619283912356,-86.9486192066278%7C40.46716415540354,-86.95429476059878%7C40.47024506180284,-86.95576733520348%7C40.47034248927443,-86.9517606080918%7C40.46857485459526,-86.94694887644629%7C40.47062085295775,-86.939740426341',
     );
   }, []);
-  //   console.log('route2.params', route2.params);
 
   const onDirection = useCallback(() => {}, []);
 
   const dispatch = useAppDispatch();
 
-  const handleWaypoint = (color: string) => {
-    if (color == 'Blue') {
+  const handleWaypoint = (type: string) => {
+    if (type == 'Safe') {
       dispatch(
-        waypointSlice.actions.setWaypoint({
+        waypointSlice.actions.setWaypointChecked({
           wayPointChecked: [true, false, false, false],
         }),
       );
     }
-    if (color == 'Green') {
+    if (type == 'Safetest') {
       dispatch(
-        waypointSlice.actions.setWaypoint({
+        waypointSlice.actions.setWaypointChecked({
           wayPointChecked: [false, true, false, false],
         }),
       );
     }
-    if (color == 'Orange') {
+    if (type == 'Short') {
       dispatch(
-        waypointSlice.actions.setWaypoint({
+        waypointSlice.actions.setWaypointChecked({
           wayPointChecked: [false, false, true, false],
         }),
       );
     }
-    if (color == 'Red') {
+    if (type == 'Shortest') {
       dispatch(
-        waypointSlice.actions.setWaypoint({
+        waypointSlice.actions.setWaypointChecked({
           wayPointChecked: [false, false, false, true],
         }),
       );
@@ -93,96 +79,168 @@ const DirectionSheet = ({route}: DircetionSheetProps) => {
 
   return (
     <View style={styles.container}>
-      <Text
-        style={
-          styles.headerText
-        }>{`From: ${departureName}\nTo: ${arrivalName}`}</Text>
+      <Text style={styles.headerText}>
+        {`From: ${departureName}\nTo: ${arrivalName}`}
+      </Text>
+      <Image source={require('../assets/line.png')} style={styles.line} />
       <View style={styles.buttonContainer}>
-        <View
-          style={
-            waypointChecked[0]
-              ? StyleSheet.compose(styles.routeButton, styles.routeButtonActive)
-              : styles.routeButton
-          }>
-          <Text
+        <View style={styles.naviContainer}>
+          <View
             style={
               waypointChecked[0]
                 ? StyleSheet.compose(
-                    styles.routeButtonText,
-                    styles.routeButtonTextActive,
+                    styles.routeButton,
+                    styles.routeButtonActive,
                   )
-                : styles.routeButtonText
-            }
-            onPress={() => {
-              handleWaypoint('Blue');
-            }}>
-            Blue{'\n'}Route
-          </Text>
+                : styles.routeButton
+            }>
+            <Text
+              style={
+                waypointChecked[0]
+                  ? StyleSheet.compose(
+                      styles.routeButtonText,
+                      styles.routeButtonTextActive,
+                    )
+                  : styles.routeButtonText
+              }
+              onPress={() => {
+                handleWaypoint('Safe');
+              }}>
+              Safe{'\n'}Route
+            </Text>
+          </View>
+          <View style={styles.naviButton}>
+            <Image
+              source={require('../assets/naviColor.png')}
+              style={styles.naviIcon}
+            />
+            <Text
+              style={styles.naviButtonText}
+              onPress={() => {
+                onNavigation();
+              }}>
+              Navi
+            </Text>
+          </View>
         </View>
-        <View
-          style={
-            waypointChecked[1]
-              ? StyleSheet.compose(styles.routeButton, styles.routeButtonActive)
-              : styles.routeButton
-          }>
-          <Text
+
+        <View style={styles.naviContainer}>
+          <View
             style={
               waypointChecked[1]
                 ? StyleSheet.compose(
-                    styles.routeButtonText,
-                    styles.routeButtonTextActive,
+                    styles.routeButton,
+                    styles.routeButtonActive,
                   )
-                : styles.routeButtonText
-            }
-            onPress={() => {
-              handleWaypoint('Green');
-            }}>
-            Green{'\n'}Route
-          </Text>
+                : styles.routeButton
+            }>
+            <Text
+              style={
+                waypointChecked[1]
+                  ? StyleSheet.compose(
+                      styles.routeButtonText,
+                      styles.routeButtonTextActive,
+                    )
+                  : styles.routeButtonText
+              }
+              onPress={() => {
+                handleWaypoint('Safest');
+              }}>
+              Safetest{'\n'}Route
+            </Text>
+          </View>
+          <View style={styles.naviButton}>
+            <Image
+              source={require('../assets/naviColor.png')}
+              style={styles.naviIcon}
+            />
+            <Text
+              style={styles.naviButtonText}
+              onPress={() => {
+                onNavigation();
+              }}>
+              Navi
+            </Text>
+          </View>
         </View>
-        <View
-          style={
-            waypointChecked[2]
-              ? StyleSheet.compose(styles.routeButton, styles.routeButtonActive)
-              : styles.routeButton
-          }>
-          <Text
+        <View style={styles.naviContainer}>
+          <View
             style={
               waypointChecked[2]
                 ? StyleSheet.compose(
-                    styles.routeButtonText,
-                    styles.routeButtonTextActive,
+                    styles.routeButton,
+                    styles.routeButtonActive,
                   )
-                : styles.routeButtonText
-            }
-            onPress={() => {
-              handleWaypoint('Orange');
-            }}>
-            Orange{'\n'}Route
-          </Text>
+                : styles.routeButton
+            }>
+            <Text
+              style={
+                waypointChecked[2]
+                  ? StyleSheet.compose(
+                      styles.routeButtonText,
+                      styles.routeButtonTextActive,
+                    )
+                  : styles.routeButtonText
+              }
+              onPress={() => {
+                handleWaypoint('Short');
+              }}>
+              Short{'\n'}Route
+            </Text>
+          </View>
+          <View style={styles.naviButton}>
+            <Image
+              source={require('../assets/naviColor.png')}
+              style={styles.naviIcon}
+            />
+            <Text
+              style={styles.naviButtonText}
+              onPress={() => {
+                onNavigation();
+              }}>
+              Navi
+            </Text>
+          </View>
         </View>
-        <View
-          style={
-            waypointChecked[3]
-              ? StyleSheet.compose(styles.routeButton, styles.routeButtonActive)
-              : styles.routeButton
-          }>
-          <Text
+        <View style={styles.naviContainer}>
+          <View
             style={
               waypointChecked[3]
                 ? StyleSheet.compose(
-                    styles.routeButtonText,
-                    styles.routeButtonTextActive,
+                    styles.routeButton,
+                    styles.routeButtonActive,
                   )
-                : styles.routeButtonText
-            }
-            onPress={() => {
-              handleWaypoint('Red');
-            }}>
-            Red{'\n'}Route
-          </Text>
+                : styles.routeButton
+            }>
+            <Text
+              style={
+                waypointChecked[3]
+                  ? StyleSheet.compose(
+                      styles.routeButtonText,
+                      styles.routeButtonTextActive,
+                    )
+                  : styles.routeButtonText
+              }
+              onPress={() => {
+                handleWaypoint('Shortest');
+              }}>
+              Shortest{'\n'}Route
+            </Text>
+          </View>
+          <View style={styles.naviButton}>
+            <Image
+              source={require('../assets/naviColor.png')}
+              style={styles.naviIcon}
+            />
+            <Text
+              style={styles.naviButtonText}
+              onPress={() => {
+                onNavigation();
+              }}>
+              Navi
+            </Text>
+          </View>
         </View>
-        <Image source={require('../assets/line.png')} style={styles.line} />
       </View>
     </View>
   );
@@ -204,6 +262,9 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
     justifyContent: 'center',
   },
+  naviContainer: {
+    display: 'flex',
+  },
   routeButton: {
     display: 'flex',
     flexDirection: 'row',
@@ -221,6 +282,16 @@ const styles = StyleSheet.create({
     shadowRadius: 2,
     shadowOpacity: 0.6,
   },
+  naviButton: {
+    display: 'flex',
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    width: 70,
+    marginHorizontal: 10,
+    paddingVertical: 4,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   routeButtonActive: {
     backgroundColor: '#f4511e',
   },
@@ -230,17 +301,23 @@ const styles = StyleSheet.create({
     fontSize: 14,
     fontWeight: 'bold',
   },
+  naviButtonText: {
+    color: 'black',
+    textAlign: 'center',
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
   routeButtonTextActive: {
     color: 'white',
   },
   naviIcon: {
-    width: 15,
-    height: 18,
+    width: 10,
+    height: 13,
     marginRight: 5,
   },
   line: {
-    width: 280,
-    margin: 15,
+    width: '100%',
+    marginVertical: 7,
   },
 });
 
